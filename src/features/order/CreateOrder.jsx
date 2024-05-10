efe6959335ca862288e03e58c14d0075a0756730
Alex <147496294+A-Hoenig@users.noreply.github.com>
2024-05-10 16:36:27 +0200

    style buttons and loader

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
+import Button from "../../ui/Button";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -48,13 +49,13 @@ function CreateOrder() {
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input className="input" type="text" name="customer" required />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input className="input" type="tel" name="phone" required />
           </div>
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
@@ -62,7 +63,7 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input type="text" name="address" required className="input" />
           </div>
         </div>
 
@@ -71,6 +72,12 @@ function CreateOrder() {
             type="checkbox"
             name="priority"
             id="priority"
+            className="h-6 w-6 accent-yellow-400 focus:outline-none
+            focus:ring
+            focus:ring-yellow-400
+            focus:ring-offset-2
+            transition-all
+            duration-300"
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
@@ -79,30 +86,9 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button
-            disabled={isSubmitting}
-            className="
-            bg-yellow-400 
-            uppercase 
-            font-semibold 
-            text-stone-800 
-            py-3 px-4 
-            inline-block 
-            tracking-wide 
-            rounded-full
-             hover:bg-yellow-300 
-             duration-300 
-             transition-colors 
-             focus:outline-none 
-             focus:ring 
-             focus:ring-yellow-400 
-             focus:bg-yellow-300 
-             focus:ring-offset-2
-             disabled:cursor-not-allowed
-             "
-          >
+          <Button disabled={isSubmitting}>
             {isSubmitting ? "Placing order ... " : "Order now"}
-          </button>
+          </Button>
         </div>
       </Form>
     </div>
--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -16,6 +16,22 @@ function SearchOrder() {
         placeholder="Search for Order#"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="
+        rounded-full
+        bg-yellow-100
+        px-4 py-2
+        text-sm
+        placeholder:text-stone-400
+        w-28
+        sm:w-64
+        sm:focus:w-72
+        transition-all 
+        duration-300
+        focus:outline-none
+        focus:ring
+        focus:ring-yellow-600
+        focus:ring-opacity-50
+        "
       />
     </form>
   );
